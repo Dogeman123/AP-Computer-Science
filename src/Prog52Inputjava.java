c4faca1f37c3ad2f56975430640da178c6233ed7
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-09-09 10:30:16 -0500

    update

--- src/Prog52Inputjava.java
+++ src/Prog52Inputjava.java
@@ -8,7 +8,8 @@ public class Prog52Inputjava {
             System.out.print("Please enter the length ");
             String len = input.nextLine();
 
-            int len = input.nextInt();
+            int leng = input.nextInt();
+
 
            System.out.print("Please enter the length");
             System.out.println();
@@ -17,10 +18,10 @@ public class Prog52Inputjava {
 
             System.out.print("Please enter the width ");
 
-            int area = len * wid;
-            int perim = 2 * len + 2 * wid;
+            int area = leng * wid;
+            int perim = 2 * leng + 2 * wid;
             System.out.println("the area is " + area);
-            System.out.println("the perim is " +perim);
+            System.out.println("the perim is " + perim);
         }
     }
 
