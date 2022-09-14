99c9967801b32a7db8299e00cc7774aeeb7c1123
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-09-14 10:21:38 -0500

    update

--- src/Prog54c.java
+++ src/Prog54c.java
@@ -1,5 +1,6 @@
 import java.util.*;
-
+// Michael verdin
+//09/14/22
 public class Prog54c {
     public static void main(String[] args){
         Scanner Keyboard = new Scanner(System.in);
@@ -10,14 +11,31 @@ public class Prog54c {
         System.out.print("Enter the are of the circle ");
         double area = Keyboard.nextDouble();
 
+        double circum = 2*3.14159*Radius;
+        System.out.println("The circumference of the circle is " + circum);
 
 
 
 
 
 
+    /*
+public class Prog54c {
+    public static void main(String[] args){
+        Scanner Keyboard = new Scanner(System.in);
 
+        System.out.print("Enter the Radius of the circle ");
+        double Radius = Keyboard.nextDouble();
 
+        System.out.print("Enter the are of the circle ");
+        double area = Keyboard.nextDouble();
+
+        double circum = 2*3.14159*Radius;
+        System.out.println("The circumference of the circle is " + circum);
+
+good
+
+     */
 
     }
 }
