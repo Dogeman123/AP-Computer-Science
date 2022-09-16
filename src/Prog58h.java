6aa9908d106bfcd48d20e5d9bcadfd551d416e4f
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-09-16 10:27:35 -0500

    update

---
+++ src/Prog58h.java
@@ -0,0 +1,20 @@
+import java.util.*;
+//Michael Verdin
+// 09/13/22
+public class Prog58h {
+    public static void main(String[] args){
+        Scanner Keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the amount saved ");
+        double Saved = Keyboard.nextDouble();
+
+        System.out.print("Enter the rate ");
+        double Rate = Keyboard.nextDouble();
+
+        System.out.print("How many months ");
+        double months = Keyboard.nextDouble();
+
+        //double interest = Saved *((1+((.01*Rate)/months)(365*months/365)));
+
+    }
+}
