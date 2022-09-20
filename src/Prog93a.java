eae4992be90d44fcf5103475b7b6895d06c671d2
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-09-20 10:23:12 -0500

    update

--- src/Prog93a.java
+++ src/Prog93a.java
@@ -19,20 +19,42 @@ public class Prog93a {
 
         System.out.println("---------------------");
 
-        double Base = Math.round( KWH * .475);
+        double price = 4.75;
+
+        double Base = Math.round(price* 10 );
+
 
         System.out.println("Base Rate " + KWH + " @$ " + "0.0475" + "$ " + Base );
 
-        double Sure = Math.round(KWH* 0.0475);
-        System.out.println("Surcharge " + Sure);
+        double Sure = (price);
+        System.out.println("Surcharge " + "$" + Sure);
 
-        double City = Math.round(KWH * 0.001475);
+        double City = Math.round(price * .3);
         System.out.println("Citytax "+ "$" + City);
 
 
-       // System.out.println("Pay this amount" +);
+        double total = Base + Sure + City;
 
+        double late = total* .03;
+       System.out.println("Pay this amount $ " + total );
 
+       System.out.println("The late fee is $ " + (late + total)  );
+
+    /*
+    Enter the KWH 993
+COMP SCI Electric
+---------------------
+Kilowatts used 993.0
+---------------------
+Base Rate 993.0 @$ 0.0475$ 48.0
+Surcharge $4.75
+Citytax $1.0
+Pay this amount $ 53.75
+The late fee is $ 55.3625
+
+Process finished with exit code 0
+good
+     */
 
 
 
