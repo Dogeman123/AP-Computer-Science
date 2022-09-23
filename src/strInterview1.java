1fd5be6a9755977dd64fd604c17845931dfa1a5f
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-09-23 10:31:04 -0500

    update

---
+++ src/strInterview1.java
@@ -0,0 +1,32 @@
+// Michael verdin
+
+// 9/23/22
+
+// String Interview1
+
+public class strInterview1{
+    public static void main(String[] args){
+        String str = "beautiful beach";
+        System.out.println("The string is : " + str);
+        System.out.print("Duplicate characters: ");
+
+        for (int lcv = 0; lcv < str.length(); lcv++){
+            for (int lcv2 = lcv + 1; lcv2 <str.length(); lcv2++){
+                char letter1 = str.charAt(lcv);
+                char letter2 = str.charAt(lcv2);
+
+                if (letter1 == letter2){
+                    System.out.print(letter2 + " ");
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
+
+/*
+The string is : beautiful beach
+Duplicate characters: b e a u
+ */
