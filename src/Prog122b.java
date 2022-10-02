5a8d5f3a4a6d458cc6d6de8000be4deb5f31dd05
MichaelV <verdin.m@stu.janesville.k12.wi.us>
2022-10-01 20:17:36 -0500

    sw

---
+++ src/Prog122b.java
@@ -0,0 +1,21 @@
+public class Prog122b {
+    public static void main(String[] args){
+
+        int Hours = 0;
+        int pay = 0;
+
+
+
+        while (Hours< 40) {
+            Hours +=1;
+            System.out.println(Hours);
+
+        }
+
+
+
+
+
+    }
+
+}
