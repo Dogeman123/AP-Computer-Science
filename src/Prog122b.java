97c5c4042f664cbd33e9f9382810247fb9d377bc
MichaelV <verdin.m@stu.janesville.k12.wi.us>
2022-10-01 20:32:26 -0500

    sw

--- src/Prog122b.java
+++ src/Prog122b.java
@@ -1,32 +1,65 @@
-<<<<<<< HEAD
+// Michael Verdin
+
+// 9/18/22
 public class Prog122b {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int Hours = 0;
         int pay = 0;
 
+        System.out.print("Hours" + "    " + "Pay");
 
-
-        while (Hours< 40) {
-            Hours +=1;
-            System.out.println(Hours);
+        while (Hours < 40) {
+            Hours += 1;
+            pay = Hours* 4;
+            System.out.println(Hours + "   " + pay);
 
         }
 
 
-
-
-
     }
-
-=======
-// Michael verdin
-
-// 9/26/22
-
-public class Prog122b {
-    public static void main(String[] args){
-
-    }
->>>>>>> origin/master
 }
+
+/*
+Hours    Pay
+1   4
+2   8
+3   12
+4   16
+5   20
+6   24
+7   28
+8   32
+9   36
+10   40
+11   44
+12   48
+13   52
+14   56
+15   60
+16   64
+17   68
+18   72
+19   76
+20   80
+21   84
+22   88
+23   92
+24   96
+25   100
+26   104
+27   108
+28   112
+29   116
+30   120
+31   124
+32   128
+33   132
+34   136
+35   140
+36   144
+37   148
+38   152
+39   156
+40   160
+ */
