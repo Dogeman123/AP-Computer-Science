80cc2104f49d6f0f71c1329c4b7412c60723217c
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-10-04 10:30:59 -0500

    update

--- src/LP41.java
+++ src/LP41.java
@@ -14,9 +14,21 @@ import java.util.*;
         if (num <= 99) {
             num = num * .30;
 
+        } else if (num<=499){
+           num = num * .28;
+
+        }else if (num<=749) {
+            num = num * .27;
+
+        }else if (num<=1000) {
+            num = num * .26;
+        }else if (num>1000) {
+            num = num * .25;
+
         }
+
         System.out.println("Price per copy is: " + num);
-        System.out.println("total cost is:");
+            System.out.println("total cost is:");
 
     }
 }
