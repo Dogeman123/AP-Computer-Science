69f93adc2d012f2ed9daca514d55a7956dfe0dfa
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-10-10 10:30:13 -0500

    update

---
+++ src/LP46.java
@@ -0,0 +1,24 @@
+// Michael Verdin
+// 10/10/22
+// LP4-6
+import java.util.*;
+public class LP46 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+
+        Random Compnum = new Random();
+        int num = Compnum.nextInt(10);
+
+        Random Compnum2 = new Random();
+        int num2 = Compnum.nextInt(10);
+
+        Random Compnum3 = new Random();
+        int num3 = Compnum.nextInt(10,14);
+
+        System.out.print("What is: " + num  + " " + num2);
+
+
+    }
+}
