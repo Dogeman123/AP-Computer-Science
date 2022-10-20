487028e79d0ba1b2a54ca1f37422deb1479fc3f9
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-10-20 10:30:20 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

---
+++ src/LP517.java
@@ -0,0 +1,23 @@
+// Michael Verdin
+// 10/20/22
+// LP5-17
+import java. util.*;
+public class LP517 {
+    public static void main(String[] args){
+        Scanner imput = new Scanner(System.in);
+
+        int num = 0;
+
+
+
+        while(num<3){
+            num += 1;
+
+        }
+        System.out.print("Enter the password: ");
+        int pass = imput.nextInt();
+
+
+
+    }
+}
