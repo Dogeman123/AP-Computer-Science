16a4be617b68b52c9d5e809b6952e214a4e2aaac
MichaelV <verdin.m@stu.janesville.k12.wi.us>
2022-10-25 23:01:53 -0500

    sw

--- src/Prog65h.java
+++ src/Prog65h.java
@@ -1,21 +1,31 @@
-import java.util.*;
 // Michael Verdin
-
-// 9.23.22
-
+//Prog65h
+import java.util.*;
 public class Prog65h {
-    public static void main(String[] args){
-        Scanner keyboard = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter the first digit");
-        int dig1 = keyboard.nextInt();
-        System.out.println("Please enter the second digit");
-        int dig2 = keyboard.nextInt();
-        System.out.println("Please enter the third digit");
-        int dig3 = keyboard.nextInt();
+        System.out.print("Enter pounds: ");
+        int pound = input.nextInt();
 
+        System.out.print("Enter shillings: ");
+        int shill = input.nextInt();
+
+        System.out.print("Enter Pence: ");
+        int pence = input.nextInt();
+
+        int con = shill % pence;
+
+        System.out.print("Decimal pounds = " + pound + "." + con + pence);
 
 
 
     }
 }
+
+/*
+Enter pounds: 7
+Enter shillings: 17
+Enter Pence: 9
+Decimal pounds = 7.89
+ */
