d8576c6ed8e528edf538b6681d9ef9051a4a85c6
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-10-31 10:30:40 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Cl82a.java
+++ src/Cl82a.java
@@ -6,15 +6,24 @@ public class Cl82a {
     private int speed;
     private double fine;
     private int charge;
+    private int fee;
 
-    public Cl82a(int myLimit, int mySpeed){
+    public Cl82a(int myLimit, int mySpeed) {
         limit = myLimit;
         speed = mySpeed;
         charge = mySpeed - limit;
-
-
+        fee = (charge * 5) + 20;
 
 
     }
 
+    public void setStuff() {
+
+
+    }
+
+    public int getCharge(){ return charge; }
+    public int getSpeed() {return speed; }
+    public int getFee() {return  fee;}
+    public int getLimit() {return limit;}
 }
