740f761c850345ccbc2998c75ff3bc161770e71f
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-11-01 10:26:54 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/dfile.java
+++ src/dfile.java
@@ -1,10 +1,13 @@
 import java.util.*;
 import java.io.*;
-
 public class dfile {
     public static void main(String[] args){
         try {
-            Scanner imput = new Scanner(new File("data/prog285b.dat"));
+            Scanner input = new Scanner(new File("data/prog285b.dat"));
+
+
+
+
         } catch (IOException e){
             System.out.println("Can't find the data file");
         }
