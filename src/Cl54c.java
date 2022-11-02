a43d120b11e568dc5968a07eebfb5be716fc714e
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-11-02 10:47:38 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Cl54c.java
+++ src/Cl54c.java
@@ -1,4 +1,7 @@
 // Michael Verdin
 // prog54cClass
 public class Cl54c {
+    private double Radius;
+    private double Area;
+    private double Circum;
 }
--- src/Prog54cClass.java
+++ src/Prog54cClass.java
@@ -1,5 +1,6 @@
 // Michael Verdin
 // prog54cClass
+import java.util.*;
 public class Prog54cClass {
     public static void main(String[] args) {
 
