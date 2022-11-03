37f1d163448aeacd86d32aee615c8736393c23b1
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-11-03 10:10:36 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Prog54cClass.java
+++ src/Prog54cClass.java
@@ -3,6 +3,27 @@
 import java.util.*;
 public class Prog54cClass {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int rad = 3;
+
+        Cl54c wow = new Cl54c(rad);
+
+        wow.setStuff();
+
+
+        double area = wow.getArea();
+        double circum = wow.getCircum();
+
+        System.out.println("The radius of the circle is: " + rad);
+        System.out.println("The Area of the circle = " + area);
+        System.out.println("The circumference of the circle = " + circum);
+
 
     }
 }
+/*
+The radius of the circle is: 3
+The Area of the circle = 28.273500000000002
+The circumference of the circle = 18.849539999999998
+ */
