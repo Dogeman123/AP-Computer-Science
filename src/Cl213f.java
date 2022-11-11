7d0cfe90325b70e05de1554775ad33f9d2cfe46f
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-11-11 10:30:30 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Cl213f.java
+++ src/Cl213f.java
@@ -16,15 +16,16 @@ public class Cl213f {
 
             if (myKwh < 8000) {
                 myCost = myKwh * .5;
-
-                if (myKwh > 10000) {
-                    myCost = myKwh * .4;
-                }
-
             }
-            //public String toString(); {
-               //     return "The cost of";
-
+            if (myKwh > 10000) {
+                myCost = myKwh * .4;
             }
         }
     }
+
+    public String toString() {
+
+        return "The cost of " + myKwh + " is " + myCost;
+    }
+}
+
--- src/Prog213fArray.java
+++ src/Prog213fArray.java
@@ -1,12 +1,14 @@
 // Michael verdin
 // Prog213fArray
 //11/8/22
-import java.util.*;
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Prog213fArray {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         try {
-            Scanner input = new Scanner(new File("data/prog213f.dat"));
+            Scanner input = new Scanner(new File("data/prog215a.dat"));
 
             Cl213f[] list = new Cl213f[1000];
             int cnt = 0;
@@ -15,22 +17,19 @@ public class Prog213fArray {
                 int kwh = input.nextInt();
 
                 Cl213f yikes = new Cl213f(kwh);
-                list[cnt]= yikes;
+                list[cnt] = yikes;
                 cnt++;
             }
 
-            for (int lcv =0; lcv <100; lcv++)
+            for (int lcv = 0; lcv < cnt; lcv++)
                 list[lcv].calc();
 
-            for (int lcv =0; lcv <100; lcv++)
-               System.out.println(list[cnt]);
+            for (int lcv = 0; lcv < cnt; lcv++)
+                System.out.println(list[lcv]);
             // System.out.println(list[lcv].toString());
 
-
-
-        } catch (IOException e){
-            System.out.println("Can't find the data file");
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
         }
-
     }
 }
