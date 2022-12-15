c756f7b63025060c04539eeee5172eceb57451e6
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2022-12-15 10:31:25 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Cl214c.java
+++ src/Cl214c.java
@@ -1,6 +1,30 @@
 public class Cl214c {
-    private String Gas;
-    private double cost;
-    private double Gcost;
+    private String myGas;
+    private double myCost;
+    private double myGallons;
+    private double myGcost;
+    private double myWash;
+    private double myTotal;
 
+    public Cl214c(String Gas,double Cost,double Gallons, double Gcost, double Wash, double Total){
+        myGas = Gas;
+        myCost = 0;
+       myGallons = Gallons;
+        myGcost = 0;
+        myWash = Wash;
+        myTotal = 0;
+
+    }public void setStuff(){
+        if(myGas == "Premium"){
+            myCost = 1.479;
+
+        }if (myGas == "Regular"){
+            myCost = 1.359;
+
+        } else{
+            myCost = 1.429;
+        }
+
+
+    }
 }
