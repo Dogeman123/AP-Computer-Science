2706982fb4c4e0efa71cfebd5aa14ed437f00a83
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2023-01-03 10:31:06 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

---
+++ src/Cl215i.java
@@ -0,0 +1,13 @@
+// Michael Verdin
+// Prog215i
+public class Cl215i {
+    private int myBest;
+    private int myWorst;
+    private double myavg;
+    private int myNumV;
+
+    public Cl505a(int best, int Worst){
+        myBest = best;
+
+    }
+}
---
+++ src/Prog215i.java
@@ -0,0 +1,19 @@
+// Michael Verdin
+// Prog215i
+import java.util.*;
+import java.io.IOException;
+import java.io.File;
+public class Prog215i {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(new File("data/prog285b.dat"));
+
+
+
+        } catch (IOException e) {
+            System.out.println("Can't find data file!");
+        }
+    }
+}
+
+
