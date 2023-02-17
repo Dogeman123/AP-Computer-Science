69f91350763f720f45ed4c535a3928700de0f289
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2023-02-17 09:43:49 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Prog702q/Prog702q.java
+++ src/Prog702q/Prog702q.java
@@ -7,7 +7,7 @@ import Prog702p.animals;
 import java.util.*;
 import java.io.IOException;
 import java.io.File;
-public class Prog702q {
+public class Prog702q{
     public static void main(String[] args) {
         try {
             Scanner input = new Scanner(new File("data/702q.dat"));
