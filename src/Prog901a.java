bb5a7f3b0d52bfe86c01a500a21a4ed47d9f9091
mverdin <verdin.n@stu.janesville.k12.wi.us>
2023-03-30 10:28:58 -0500

    Electric Eel

--- src/Prog901a.java
+++ src/Prog901a.java
@@ -17,7 +17,7 @@ import java.io.File;
             if (s == null || s.length() <= 1) {
                 return s;
             }
-            return reverseString(s.substring(1))+s.charAt(0);
+             return reverseString(s.substring(1))+s.charAt(0);
         }
 
         public static void main(String[] args) {
