07bd3b303a5793f3d0b30b742dd9eda3afee5767
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2023-05-01 10:28:36 -0500

    Boar
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/StegLab/SteganographyLab/StegStudentCode/Steganography.java
+++ src/StegLab/SteganographyLab/StegStudentCode/Steganography.java
@@ -1,28 +1,26 @@
 package StegLab.SteganographyLab.StegStudentCode;
+import java.awt.Color;
 
 public class Steganography extends Picture {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+public static void clearLow(Pixel p){
+   
+}
 
 
 
 
 
     public static void main(String[] args) {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        Picture copy = testClearLOw(beach);
+        copy.explore();
+    }
+
+    public static Picture testClearLOw(Picture p) {
 
     }
 }
