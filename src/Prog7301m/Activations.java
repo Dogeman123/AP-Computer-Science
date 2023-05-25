b46eb8e8edbc28595e919fa815172efed6f5de14
verdin.m <verdin.m@stu.janesville.k12.wi.us>
2023-05-25 10:24:28 -0500

    Boar
    # Conflicts:
    #	out/production/Ap Computer Science/LP46.class
    #	src/LP46.java
    #	src/LP59.java

--- src/Prog7301m/Activations.java
+++ src/Prog7301m/Activations.java
@@ -7,16 +7,17 @@ public class Activations {
     }
 
     public static class Sigmoid implements Activation {
-        public double activate(double value) { return 1 / (1 + Math.pow(Math.E, -value));}
-        public double activateDeriv(Double value) { return (value - Math.pow(value, 2));}
+        public double activate(double value) { return 1/(1+Math.pow(Math.E, -value)); }
+        public double activateDeriv(double value) { return (value-Math.pow(value, 2)); }
     }
 
     public static class Tanh implements Activation {
-        public double activate(double value) { return Math.tanh(value);}
-        public double activateDeriv(Double value) { return 1 - Math.pow(value, 2);}
+        public double activate(double value) { return Math.tanh(value); }
+        public double activateDeriv(double value) { return 1-Math.pow(value, 2); }
     }
+
     public static class ReLU implements Activation {
-        public double activate(double value) { return Math.max(0,value);}
-        public double activateDeriv(Double value) { return value >= 0 ? 1 :0;}
+        public double activate(double value) { return Math.max(0, value); }
+        public double activateDeriv(double value) { return value >= 0 ? 1 : 0; }
     }
 }
