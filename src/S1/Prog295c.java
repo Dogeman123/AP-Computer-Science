eb48a84b12b9685ca9b15637ae259f5fcaeba6a0
mverdin <verdin.n@stu.janesville.k12.wi.us>
2023-06-05 12:39:56 -0500

    Electric Eel

--- src/S1/Prog295c.java
+++ src/S1/Prog295c.java
@@ -1,11 +1,60 @@
-package S1;//Michael Verdin
-//S1.Prog295c
+package S1;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Prog295c {
 
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner input1 = new Scanner(new File("merge1.dat"));
+        Scanner input2 = new Scanner(new File("merge2.dat"));
+
+        System.out.println("social sec#  Class Type Dept");
+
+        String line1 = getNextLine(input1);
+        String line2 = getNextLine(input2);
+
+        while (line1 != null || line2 != null) {
+
+            if (line1 == null) {
+                printEmployee(line2);
+                line2 = getNextLine(input2);
+
+            } else if (line2 == null) {
+                printEmployee(line1);
+                line1 = getNextLine(input1);
+
+            } else if (getSSN(line1).compareTo(getSSN(line2)) < 0) {
+                printEmployee(line1);
+
+                line1 = getNextLine(input1);
+
+            } else {
+                printEmployee(line2);
+                line2 = getNextLine(input2);
+            }
+        }
+    }
+
+    private static String getNextLine(Scanner input) {
+        if (input.hasNextLine()) {
+            return input.nextLine();
+        } else {
+            return null;
+        }
+    }
+
+    private static String getSSN(String line) {
+        return line.substring(0, line.length()-1);
+    }
+
+    private static void printEmployee(String line) {
+        String ssn = getSSN(line);
+        String clas = line.substring(12, line.length()-1);
+        String type = line.substring(14, line.length()-1);
+        String dept = line.substring(line.length()-1);
+
+        System.out.printf(ssn, clas, type, dept);
+    }
 }
